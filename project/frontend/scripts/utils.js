46bd3865b8743555d4985bcc0074e95a58299927
Guilherme Borges <guilhermeborges.developer@gmail.com>
2026-04-09 23:27:52 -0300

    ajuste pega senha, e add formulario edicao

--- project/frontend/scripts/utils.js
+++ project/frontend/scripts/utils.js
@@ -12,10 +12,19 @@ export async function abrirModalFormulario(elemento, formulario) {
 }
 
 //aqui ele monta a funcao que vai abrir o dialog de edicao dos usuario
-export async function abrirModalFormularioEdicao(elemento, formulario) {
+export async function abrirModalFormularioEdicao(elemento, formulario, dados) {
     const formulario_elemento = document.getElementById(formulario) 
     //aqui vai deixar o formulario em branco
     formulario_elemento.reset();
+
+    try {
+        formulario_elemento.elements['nome'].value = dados.Nome
+        formulario_elemento.elements['login'].value = dados.Login
+        formulario_elemento.elements['senha'].value = dados.Senha
+        formulario_elemento.elements['permissao'].value = dados.Permissao
+    } catch (e) {
+
+    }
 
     const tela = document.getElementById(elemento);
     //aqui ele deixa visivel o modal
